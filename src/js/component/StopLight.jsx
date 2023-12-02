fd0c7c8a963319c98c52b1e2f66409c6c3a10f1f
Arcarius41 <145110529+Arcarius41@users.noreply.github.com>
2023-12-02 23:55:49 +0000

    think its finished

--- src/js/component/StopLight.jsx
+++ src/js/component/StopLight.jsx
@@ -1,25 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-export default function StopLight(){
-let [active, setActive] = useState("red");
-return(
-    <div>
-        <div className="top"></div>
-        <div className="traffic-light">
-            <div>
-                className={active === "red" ? "red light glow" : "red light" }
-                onClick{() => setActive("red")}
+export default function StopLight() {
+    let [active, setActive] = useState("red");
+    return (
+        <div>
+            <div className="top"></div>
+            <div className="traffic-light">
+                <div className={active === "red" ? "red light glow" : "red light"}
+                    onClick={() => setActive("red")}>
+
+                </div>
+                <div className={active === "yellow" ? "yellow light glow" : "yellow light"}
+                    onClick={() => setActive("yellow")}>
+
+                </div>
+                <div className={active === "green" ? "green light glow" : "green light"}
+                    onClick={() => setActive("green")}>
+
+                </div>
             </div>
-            <div>
-                className={active === "yellow" ? "yellow light glow" : "yellow light" }
-                onClick{() => setActive("yellow")}
-            </div>
-            <div>
-                    className={active === "green" ? "green light glow" : "green light" }
-                    onClick{() => setActive("green")}
-            </div>
-        </div>
-    </div>);
+        </div>);
 
 
 }
